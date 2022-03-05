1a937aeacc2156c347f5f689cf4a7f1bd8e1f0cd
ilhammuksidin <ilham.muksidin@dana.id>
2022-03-05 07:54:37 +0700

    Add web auto init

---
+++ cypress-api-test/cypress/integration/tests/basic.spec.js
@@ -0,0 +1,13 @@
+context('web automation testing', () => {
+
+    beforeEach(() => {
+        cy.visit('https://flip.id/')
+      })
+
+    it("Find out where Flip generates revenue, make sure it mentioned Flip Plus, Big Flip and Digital Product", () => {
+
+  
+
+    })
+
+});
